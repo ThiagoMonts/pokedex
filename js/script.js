df350f68039420346ae40fe870fad38ff7c10852
ThiagoMonts <thiagohonorato@hotmail.com>
2022-10-15 10:40:09 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,18 +42,18 @@ const renderPokemon = async (pokemon) => {
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     renderPokemon(input.value.toLowerCase());
-})
+});
 
 buttonPrev.addEventListener('click', () => {
     if (searchPokemon > 1) {
         searchPokemon -= 1;
         renderPokemon(searchPokemon);  
     }
-})
+});
 
 buttonNext.addEventListener('click', () => {
     searchPokemon += 1;
     renderPokemon(searchPokemon);    
-})
+});
 
 renderPokemon(searchPokemon);
